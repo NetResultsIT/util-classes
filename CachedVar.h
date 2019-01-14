NetResultsIT/util-classes

commit 750851fb630086f1085b8094ab8a06b286d88a68

Also check for initialized var during comparison

CachedVar.h
@@ -50,21 +50,17 @@ public:
     /*
      *  comparison
      */
-    bool operator ==(const T& rhs) const
-    {
-        return ( this->m_currentVal == rhs ) ? true : false;
-    }
-    bool operator !=(const T& rhs) const { return (this->m_currentVal != rhs); }
+    bool operator ==(const T& rhs) const { return m_initialized && ( this->m_currentVal == rhs ); }
+    bool operator !=(const T& rhs) const { return !m_initialized || ( this->m_currentVal != rhs ); }
 
     inline bool isEmpty() const { return !m_initialized; }
-    inline bool changed() const
+    inline bool changed() const { return m_initialized && ( m_previousVal == m_currentVal ); }
+    void restore()
     {
-        return (this->m_previousVal == this->m_currentVal) ? false : true;
-    }
-    bool restore()
-    {
-        if (!m_initialized) return false;
-        m_currentVal = m_previousVal;
+        if (m_initialized)
+        {
+            m_currentVal = m_previousVal;
+        }
     }
 
     inline T current() const  { return m_currentVal;  }

commit 466835d869b2d0074904cf1bcf2c2d23947fcd51

fix changed() method broken in 750851fb

CachedVar.h
@@ -54,7 +54,7 @@ public:
     bool operator !=(const T& rhs) const { return !m_initialized || ( this->m_currentVal != rhs ); }
 
     inline bool initialized() const { return m_initialized; }
-    inline bool changed() const { return m_initialized && ( m_previousVal == m_currentVal ); }
+    inline bool changed() const { return m_initialized && ( m_previousVal != m_currentVal ); }
     void restore()
     {
         if (m_initialized)